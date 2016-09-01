75c43d8fd34592120b93ae5a3d9c1ce7a200cad8
Mattias Hagberg <id14mhg@cs.umu.se>
2016-09-01 17:03:28 +0200

    Added structure

---
+++ RobotMove.py
@@ -0,0 +1,24 @@
+"""
+File handles Robot movements and maths about it.
+"""
+
+
+""" + moveToCoord(XPoint, YPoint)
+    while(inget är ivägen)
+        1: Hitta punkten som man ska sikta på.
+        2: snurra eller kör + sväng
+    end
+
+    """
+
+
+#forward
+#forwardSpeed(M/s^2)
+
+#stop
+
+#turn right(target angle)
+#turn left(target angle)
+#turn speed(M/s^2)
+
+
