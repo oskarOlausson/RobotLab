9f97da8b65c6fed51b7797eb9c1e4b54075d43d5
Mattias Hagberg <id14mhg@cs.umu.se>
2016-09-09 11:08:24 +0200

    made Path.py to a class path.py

---
+++ path.py
@@ -0,0 +1,24 @@
+"""
+File that handles paths and stuff
+"""
+
+import Trig
+import json
+
+
+class Path:
+    def __init__(self, pathName):
+        self.pathName = fileString
+        self.path = self.load(pathName)
+
+    def load(self, fileString):
+        with open(fileString, 'r') as data_file:
+            data = json.load(data_file)
+        return data
+
+    # returns the position of the given point
+    def position(self, index):
+        return self.path[index]['Pose']['Position']['X'], self.path[index]['Pose']['Position']['Y']
+
+    def length(self):
+        return len(self.path)
